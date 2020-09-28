a882a1ad0454847f3cedc896e637e68a88d8fc81
Avalanche-one <65412346+Avalanche-one@users.noreply.github.com>
2020-09-28 16:04:40 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,66 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        infinite: true,
+        arrows: false,
+        dots: true
+    });
+
+    $('.advantages__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1125,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+      ]
+    });
+
+    $('.team__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1125,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+      ]
+    });
+
+    $('.header__menu-btn').on('click', function(){
+        $('.menu__wrapper').toggleClass('active');
+      });
+      
+    $('.menu__wrapper-btn').on('click', function(){
+        $('.menu__wrapper').toggleClass('active');
+    });
+
+    new WOW().init();
+});
